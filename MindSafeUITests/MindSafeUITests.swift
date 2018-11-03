b18da70a60bba4eac5f91d5ca6e19e67e9ae1bc0
oleg <oleg@olegs-imac.local>
2018-11-02 21:04:32 -0700

    Commit for UI tests for certain requirements. Reviewed by: Mihai

--- MindSafeUITests/MindSafeUITests.swift
+++ MindSafeUITests/MindSafeUITests.swift
@@ -9,12 +9,13 @@
 import XCTest
 
 class MindSafeUITests: XCTestCase {
-        
+    var app: XCUIApplication!
     override func setUp() {
         super.setUp()
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+        app = XCUIApplication()
+        app.launch()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
@@ -28,9 +29,85 @@ class MindSafeUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    //Requirement 1.2
+    func testHomePageButtonsTabbar() {
         // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tabBarsQuery = XCUIApplication().tabBars
+        let homeButton = tabBarsQuery.buttons["Home"]
+        let remindersButton = tabBarsQuery.buttons["Reminders"]
+        //let trackerButton = tabBarsQuery.buttons["Tracker"]
+        // let activitiesButton = tabBarsQuery.buttons["Activities"]
+        let homeElement = app.navigationBars["Home"].otherElements["Home"]
+        let addReminderButton = app.navigationBars["MindSafe.RemindersView"].buttons["Add reminder"]
+        homeButton.tap()
+        XCTAssertTrue(homeElement.exists)
+        remindersButton.tap()
+        XCTAssertTrue(addReminderButton.exists)
+        homeButton.tap()
+        homeButton.tap()
+        XCTAssertTrue(homeElement.exists)
+    }
+    
+    //Requirement 3.1
+    func testNotificationCancelation() {
+        let remindersButton = app.tabBars.buttons["Reminders"]
+        let addReminderButton = app.navigationBars["MindSafe.RemindersView"].buttons["Add reminder"]
+        let cancelButton = app.tables/*@START_MENU_TOKEN@*/.buttons["Cancel"]/*[[".cells.buttons[\"Cancel\"]",".buttons[\"Cancel\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        remindersButton.tap()
+        remindersButton.tap()
+        XCTAssertTrue(addReminderButton.isHittable)
+        addReminderButton.tap()
+        XCTAssertTrue(cancelButton.isHittable)
+        cancelButton.tap()
+        XCTAssertTrue(addReminderButton.isHittable)
+    }
+    
+    //Requirement 1.1
+    func testSettings() {
+        let settingsStaticText = app.staticTexts["settings"]
+        let homeButton = app.navigationBars["UIView"].buttons["Home"]
+        let settingsButton = app.navigationBars["Home"].buttons["Settings"]
+        XCTAssertFalse(settingsStaticText.exists)
+        settingsButton.tap()
+        XCTAssertTrue(settingsStaticText.exists)
+        homeButton.tap()
+        XCTAssertFalse(settingsStaticText.exists)
+    }
+    
+    //Requirement 1.1
+    func testPanic() {
+        let button = app.navigationBars["Home"].children(matching: .button).element(boundBy: 0)
+        let alertStaticText = app.staticTexts["ALERT"]
+        let homeButton = app.navigationBars["UIView"].buttons["Home"]
+        XCTAssertFalse(alertStaticText.exists)
+        button.tap()
+        XCTAssertTrue(alertStaticText.exists)
+        homeButton.tap()
+        XCTAssertFalse(alertStaticText.exists)
+    }
+    
+    //Requirement 3.4.1
+    func testNotificationCreation() {
+        let remindersButton = app.tabBars.buttons["Reminders"]
+        remindersButton.tap()
+        remindersButton.tap()
+        app.navigationBars["MindSafe.RemindersView"].buttons["Add reminder"].tap()
+        let titleDescriptionTextField = XCUIApplication().tables/*@START_MENU_TOKEN@*/.textFields["Title & description"]/*[[".cells.textFields[\"Title & description\"]",".textFields[\"Title & description\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        titleDescriptionTextField.tap()
+        titleDescriptionTextField.tap()
+        let tKey = app/*@START_MENU_TOKEN@*/.keys["t"]/*[[".keyboards.keys[\"t\"]",".keys[\"t\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        tKey.tap()
+        let eKey = app/*@START_MENU_TOKEN@*/.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        eKey.tap()
+        let sKey = app/*@START_MENU_TOKEN@*/.keys["s"]/*[[".keyboards.keys[\"s\"]",".keys[\"s\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        sKey.tap()
+        tKey.tap()
+        let iKey = app/*@START_MENU_TOKEN@*/.keys["i"]/*[[".keyboards.keys[\"i\"]",".keys[\"i\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        iKey.tap()
+        let nKey = app/*@START_MENU_TOKEN@*/.keys["n"]/*[[".keyboards.keys[\"n\"]",".keys[\"n\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        nKey.tap()
+        app/*@START_MENU_TOKEN@*/.keys["g"]/*[[".keyboards.keys[\"g\"]",".keys[\"g\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCUIApplication().tables/*@START_MENU_TOKEN@*/.buttons["Save"]/*[[".cells.buttons[\"Save\"]",".buttons[\"Save\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
     }
     
 }
