d0dbd28f8e5459687738cae1bf1de3bb5ee2cdc4
Bob <bob@Bobs-Mac.local>
2018-11-15 17:04:20 -0800

    test

---
+++ MindSafe/TrackerInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TrackerInfoViewController.swift
+//  MindSafe
+//
+//  Created by Mihai Lapuste on 2018-11-14.
+//  Copyright © 2018 Mihai Lapuste. All rights reserved.
+//
+
+import UIKit
+
+class TrackerInfoViewController: UIViewController {
+
+    @IBAction func goBackButton(_ sender: Any) {
+        // dismiss view once notifcation is scheduled
+        dismiss(animated: true, completion: nil) // use if using modal transition
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
