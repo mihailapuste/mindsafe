a7236e3457cc330432f4e82bdc4b716a185e3904
Bob <bob@bobs-mac.local>
2018-11-17 01:12:26 -0800

    Update: Works with database sort of

--- MindSafe/ProgressViewController.swift
+++ MindSafe/ProgressViewController.swift
@@ -8,17 +8,67 @@ class ProgressViewController:
 
 UIViewController {
     
-   
+    var ref: DatabaseReference!
+    var ref2: DatabaseReference!
+    var handler:DatabaseHandle!
+    var handler2:DatabaseHandle!
+    
+    var array:[String] = []
+    var count = 0
+    
     @IBOutlet weak var LineChartView: LineChartView!
     
- 
+    @IBOutlet weak var myTextField1: UITextField!
     
-    let months = ["2018-11-1", "2018-11-2", "2018-11-3", "2018-11-4", "2018-11-5"]
-    let epScore = [20.0, 4.0, 6.0, 3.0, 12.0]
-    let semScore = [10.0, 14.0, 60.0, 13.0, 2.0]
+    
+    @IBAction func savBut1(_ sender: Any) {
+        let date = Date()
+        let formater = DateFormatter()
+        formater.dateFormat="dd.MM.yyyy"
+        let result = formater.string(from: date)
+        
+        if myTextField1.text != ""
+        {
+        ref?.child("Date1").child("\(count)").setValue(result)
+            
+            //months.append(result)
+            
+        ref?.child("epScore").child("\(count)").setValue(myTextField1.text)
+            
+            var myDouble = Double(myTextField1.text!)
+            
+            epScore.append(myDouble!)
+    
+        myDouble = myDouble!+5.0
+            
+        ref?.child("semScore").child("\(count)").setValue(String(myDouble!))
+            
+        
+            //semScore.append(myDouble!)
+            
+            myTextField1.text = ""
+            
+            count=count+1
+            
+            
+            
+        }
+        
+    
+        
+    }
+    
+    
+    @IBAction func updateChart(_ sender: Any) {
+        self.viewDidLoad()
+        self.viewWillAppear(true)
+
+    }
+    
+    var months = ["1.11.2018", "2.11.2018", "3.11.2018", "4.11.2018", "5.11.2018"]
+    var epScore = [20.0, 4.0, 6.0, 3.0, 12.0]
+    var semScore = [10.0, 14.0, 60.0, 13.0, 2.0]
     weak var axisFormatDelegate: IAxisValueFormatter?
-    
-    
     
    
     override func viewDidLoad() {
@@ -26,9 +76,77 @@ UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
+         ref=Database.database().reference()
+        
+        ref.child("Date1").observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let key = snap.key
+                let value = snap.value
+                print("key = \(key)  value = \(value!)")
+                self.months.append(value as! String)
+            }
+        })
+        
+        ref.child("epScore").observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let key = snap.key
+                let value = snap.value
+                print("key = \(key)  value = \(value!)")
+                self.epScore.append(Double(value as! String)!)
+            }
+        })
+        
+        ref.child("semScore").observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let key = snap.key
+                let value = snap.value
+                print("key = \(key)  value = \(value!)")
+                self.semScore.append(Double(value as! String)!)
+            }
+        })
         
         
         
+        
+        /*ref2=Database.database().reference()
+        handler = ref?.child("Date1").observe(.childAdded, with: {(snapshot) in
+            if let data = snapshot.value as? String
+            {
+                self.months.append(data)
+               
+                
+            }
+        })
+        
+        handler2 = ref2?.child("epScore").observe(.childAdded, with: {(snapshot) in
+            if let data1 = snapshot.value as? Double
+            {
+                self.epScore.append(data1)
+                self.viewDidLoad()
+                self.viewWillAppear(true)
+            }
+        })
+        
+        */
+        
+        
+        
+        
+        
+        //ref.child("Date1").observe(.value, with: { snapshot in
+          //  if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
+           //     for value in snapshots {
+                    //print("Child: ", value)
+            //    }
+           // }
+            
+       // })
+        
+
+
         LineChartView.delegate = self as? ChartViewDelegate
         LineChartView.noDataText = "You need to provide data for the chart."
         LineChartView.chartDescription?.text = ""
@@ -118,9 +236,6 @@ UIViewController {
         LineChartView.animate(xAxisDuration: 1.5, yAxisDuration: 1.5, easingOption: .linear)
         
         
-        
-      
-       
-        
+    
 }
 }
