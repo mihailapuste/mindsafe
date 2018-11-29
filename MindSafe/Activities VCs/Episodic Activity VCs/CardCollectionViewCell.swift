f9426a68c4302168dbf053e63b354e1501e26e77
Mihai Lapuste <mihail@ascenticorp.com>
2018-11-28 16:09:33 -0800

    Episodic game cell logic added

--- MindSafe/Activities VCs/Episodic Activity VCs/CardCollectionViewCell.swift
+++ MindSafe/Activities VCs/Episodic Activity VCs/CardCollectionViewCell.swift
@@ -13,4 +13,54 @@ class CardCollectionViewCell: UICollectionViewCell {
     @IBOutlet var frontImageView: UIImageView!
     
     @IBOutlet var backImageView: UIImageView!
+    
+    var card: Card?
+    
+    func setCard(_ card:Card) {
+        
+        // keeps track of card getting passed in
+        self.card = card
+        
+        frontImageView.image = UIImage(named: card.imageName)
+        
+        // deteremine in card is in flipped up or down state
+        
+        if card.isFlipped == true {
+            
+            // make sure frontimageview is on top
+            UIView.transition(from: backImageView, to: frontImageView, duration: 0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+            
+        }
+        else {
+            
+            // make sure backimageview is on top
+             UIView.transition(from: frontImageView, to: backImageView, duration: 0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+            
+        }
+    }
+    
+    func flip() {
+        
+        UIView.transition(from: backImageView, to: frontImageView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        
+    }
+    
+    func flipBack() {
+       
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
+            
+            UIView.transition(from: self.frontImageView, to: self.backImageView, duration: 0.3, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+            
+        }
+        
+    }
+    
+    func remove() {
+        // removes both imageviews from being visible
+        
+        // TODO: Animate action
+        backImageView.alpha = 0
+        frontImageView.alpha = 0
+        
+    }
 }
--- MindSafe/Activities VCs/Episodic Activity VCs/EpisodicViewController.swift
+++ MindSafe/Activities VCs/Episodic Activity VCs/EpisodicViewController.swift
@@ -17,6 +17,8 @@ class EpisodicViewController: UIViewController, UICollectionViewDelegate, UIColl
     var model = CardModel()
     var cardArray =  [Card]() // empty card array
     
+    var firstFlippedCardIndex: IndexPath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,24 +36,87 @@ class EpisodicViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        // get cardcollectionview cell obj
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
+        
+        // get card we are trying to display
+        let card = cardArray[indexPath.row]
+        
+        // set card in cell obj
+        cell.setCard(card)
+        
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // TODO
+       
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
+        
+        // get card we are trying to display
+        let card = cardArray[indexPath.row]
+        
+        if card.isFlipped == false {
+            
+            // flip the card
+            cell.flip()
+            card.isFlipped = true
+            
+            // determine if it is the first card or second card thats flipped over
+            if firstFlippedCardIndex == nil {
+                // this is the first card being flipped
+                firstFlippedCardIndex = indexPath
+
+            }
+            else{
+                // this is the second card being flipped
+                
+                // TODO: Perform the matching logic
+            }
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Game Logic Methods
+    
+    func checkForMatches(_ secondFlippedCardIndex: IndexPath) {
+        
+        // Get the cells from the cells for the two cards revealed
+        let cardOneCell = collectionView.cellForItem(at: firstFlippedCardIndex!) as? CardCollectionViewCell
+        
+        let cardTwoCell = collectionView.cellForItem(at: secondFlippedCardIndex) as? CardCollectionViewCell
+        
+        // get the cards from the two cards that were revealed
+        let cardOne = cardArray[firstFlippedCardIndex!.row]
+        let cardTwo = cardArray[secondFlippedCardIndex.row]
+        
+        // Compare the two cards
+        if cardOne.imageName == cardTwo.imageName {
+            
+            // its a match
+            
+            // set the statuses of the cards
+            cardOne.isMatched = true
+            cardTwo.isMatched = true
+            
+            // remove the cards from the grid
+            cardOneCell?.remove()
+            cardTwoCell?.remove()
+            
+        } else {
+            
+            // its not a match
+            
+            // set the statuses of the card
+            cardOne.isFlipped = false
+            cardTwo.isFlipped = false
+            
+            // flip both cards back
+            cardOneCell?.flipBack()
+            cardTwoCell?.flipBack()
+        }
+        
+        firstFlippedCardIndex = nil
     }
-    */
 
 }
